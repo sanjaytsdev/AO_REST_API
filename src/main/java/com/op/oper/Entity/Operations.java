117a8a4816d70b4072a69c317c2a29a408d2510c
Sanjay TS <sanjaytsdev@gmail.com>
2024-08-23 18:18:52 +0530

    Finished

---
+++ src/main/java/com/op/oper/Entity/Operations.java
@@ -0,0 +1,36 @@
+package com.op.oper.Entity;
+
+public class Operations {
+
+        private Integer a;
+        private Integer b;
+        private String op;
+        private Integer res;
+        
+        public Integer getA() {
+            return a;
+        }
+        public void setA(Integer a) {
+            this.a = a;
+        }
+        public Integer getB() {
+            return b;
+        }
+        public void setB(Integer b) {
+            this.b = b;
+        }
+        public String getOp() {
+            return op;
+        }
+        public void setOp(String op) {
+            this.op = op;
+        }
+        public Integer getRes() {
+            return res;
+        }
+        public void setRes(Integer res) {
+            this.res = res;
+        }
+        
+
+}
